8fa1294917ee676ee01a13b52bb767fbeb20ee52
Bruno Luna Ribeiro <brunoluna85@gmail.com>
2018-02-16 00:32:57 -0300

    Creation of the TemperaturaActivity and button listeners

--- app/src/main/java/br/com/posfacisa/conversordeandroid/MenuActivity.java
+++ app/src/main/java/br/com/posfacisa/conversordeandroid/MenuActivity.java
@@ -1,13 +1,26 @@
 package br.com.posfacisa.conversordeandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
+
+    private Button buttonTemperatura;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        buttonTemperatura = (Button) findViewById(R.id.buttonTemperatura);
+        buttonTemperatura.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MenuActivity.this, TemperaturaActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/br/com/posfacisa/conversordeandroid/TemperaturaActivity.java
+++ app/src/main/java/br/com/posfacisa/conversordeandroid/TemperaturaActivity.java
@@ -2,12 +2,38 @@ package br.com.posfacisa.conversordeandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class TemperaturaActivity extends AppCompatActivity {
+
+    private Button buttonInverter;
+    private TextView textViewOrigem;
+    private TextView textViewDestino;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temperatura);
+
+        textViewOrigem = (TextView) findViewById(R.id.textViewOrigem);
+        textViewDestino = (TextView) findViewById(R.id.textViewDestino);
+
+        buttonInverter = (Button) findViewById(R.id.buttonInverter);
+        buttonInverter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchText(textViewOrigem,textViewDestino);
+            }
+        });
+    }
+
+    private void switchText(TextView origem, TextView destino){
+        String nomeOrigem = origem.getText().toString();
+        String nomeDestino = destino.getText().toString();
+
+        origem.setText(nomeDestino);
+        destino.setText(nomeOrigem);
     }
 }
