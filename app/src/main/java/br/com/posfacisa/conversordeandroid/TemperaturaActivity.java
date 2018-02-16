7fce9fef8fbb06d92353e473bca0d59ad69d36fd
Bruno Luna Ribeiro <brunoluna85@gmail.com>
2018-02-15 22:42:19 -0300

    Designing the MenuActivity and adding TeperaturaActivity

---
+++ app/src/main/java/br/com/posfacisa/conversordeandroid/TemperaturaActivity.java
@@ -0,0 +1,13 @@
+package br.com.posfacisa.conversordeandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TemperaturaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_temperatura);
+    }
+}
